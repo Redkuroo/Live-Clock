393d10ee1ff145578401f86465c6ba3df5f48fa7
John Smile Mella <02jsmella@gmail.com>
2025-02-12 13:24:17 +0800

    styling left to modify

--- script.js
+++ script.js
@@ -1,24 +1,44 @@
-// Ensure Day.js is loaded
 console.log("Day.js loaded:", typeof dayjs !== "undefined");
 
-// Function to update time
+
 function updateTime() {
-    const now = dayjs();
-    document.querySelector("#current-day h1").textContent = now.format("ddd");
-    // Update time
-    document.querySelector("#current-time h1").textContent = now.format("HH:mm:ss:A");
+  const now = dayjs();
 
-    // Update date
-    document.querySelector("#current-date h1").textContent = now.format("MM/DD/YYYY");
+  document.querySelector("#current-day h1").textContent = now.format("dddd");
+  // Update time
+  document.querySelector("#current-time h1").textContent =
+    now.format("HH:mm:ss:A");
+
+  // Update date
+  document.querySelector("#current-date h1").textContent =
+    now.format("MMM DD, YYYY");
 }
-
 
 updateTime();
 setInterval(updateTime, 1000);
 
-
-
-
 MicroModal.init();
+dayjs.extend(dayjs_plugin_utc);
+dayjs.extend(dayjs_plugin_timezone);
 
+
+
+function changeTimezone() {
  
+  let selectedTimeZone = document.getElementById("time-zone").value;
+  const now = dayjs.utc();
+  const convertedTime = now.tz(selectedTimeZone).format("HH:mm:ss:A");
+ 
+  document.querySelector("#current-time h1").textContent = convertedTime;
+  setInterval(changeTimezone, 1000);
+  document.getElementById("user-timezone").textContent = selectedTimeZone;
+}
+
+document.getElementById("change").addEventListener("click", function (event) {
+    
+    event.preventDefault()
+ changeTimezone();
+
+});
+
+
